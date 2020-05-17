c3a580f0de41239a9cd541a387aa6e156d31b990
Barsil Nzola <barsiljohn@gmail.com>
2020-05-17 13:29:45 +0300

    added styling in css to hide the paragraphs in the columns and changed the class names of the paragraphs

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(".icon1").click(function(){
     $(".icon1").hide(1200);
-    $(".4p1").toggle(1200);
-    $(".4p1").show(1200);
+    $(".p1").toggle(1200);
+    $(".p1").show(1200);
   })
-  $(".4p1").click(function(){
-    $(".4p1").hide(1200);
+  $(".p1").click(function(){
+    $(".p1").hide(1200);
     $(".icon1").toggle(1200);
     $(".icon1").show(1200);
   });
@@ -13,11 +13,11 @@ $(document).ready(function(){
   $(document).ready(function(){
     $(".icon2").click(function(){
       $(".icon2").hide(1200);
-      $(".4p2").toggle(1200);
-      $(".4p2").show(1200);
+      $(".p2").toggle(1200);
+      $(".p2").show(1200);
     })
-    $(".4p2").click(function(){
-      $(".4p2").hide(1200);
+    $(".p2").click(function(){
+      $(".p2").hide(1200);
       $(".icon2").toggle(1200);
       $(".icon2").show(1200);
     });
@@ -26,11 +26,11 @@ $(document).ready(function(){
   $(document).ready(function(){
     $(".icon3").click(function(){
       $(".icon3").hide(1200);
-      $(".4p3").toggle(1200);
-      $(".4p3").show(1200);
+      $(".p3").toggle(1200);
+      $(".p3").show(1200);
     })
-    $(".4p3").click(function(){
-      $(".4p3").hide(1200);
+    $(".p3").click(function(){
+      $(".p3").hide(1200);
       $(".icon3").toggle(1200);
       $(".icon3").show(1200);
     });
