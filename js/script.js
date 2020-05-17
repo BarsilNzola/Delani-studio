06556897aef23e8a4226b0635ab9e860986db235
Barsil Nzola <barsiljohn@gmail.com>
2020-05-18 01:11:05 +0300

    added heading elements to the paragraphs in the fourth container

--- js/script.js
+++ js/script.js
@@ -1,38 +1,39 @@
 $(document).ready(function(){
   $(".icon1").click(function(){
-    $(".icon1").hide(1200);
-    $(".p1").toggle(1200);
-    $(".p1").show(1200);
-  })
-  $(".p1").click(function(){
-    $(".p1").hide(1200);
-    $(".icon1").toggle(1200);
-    $(".icon1").show(1200);
+      $(".icon1").hide();
+      $(".p1").toggle();
+      $(".p1").show();
   });
+  $(".p1").click(function(){
+    $(".p1").hide();
+    $(".icon1").toggle();
+    $(".icon1").show();
+  });
+});
 
 $(document).ready(function(){
     $(".icon2").click(function(){
-      $(".icon2").hide(1200);
-      $(".p2").toggle(1200);
-      $(".p2").show(1200);
-    })
+      $(".icon2").hide();
+      $(".p2").toggle();
+      $(".p2").show();
+    });
     $(".p2").click(function(){
-      $(".p2").hide(1200);
-      $(".icon2").toggle(1200);
-      $(".icon2").show(1200);
+      $(".p2").hide();
+      $(".icon2").toggle();
+      $(".icon2").show();
     });
 });
 
 $(document).ready(function(){
     $(".icon3").click(function(){
-      $(".icon3").hide(1200);
-      $(".p3").toggle(1200);
-      $(".p3").show(1200);
-    })
+      $(".icon3").hide();
+      $(".p3").toggle();
+      $(".p3").show();
+    });
     $(".p3").click(function(){
-      $(".p3").hide(1200);
-      $(".icon3").toggle(1200);
-      $(".icon3").show(1200);
+      $(".p3").hide();
+      $(".icon3").toggle();
+      $(".icon3").show();
     });
 });
 
@@ -42,13 +43,16 @@ $(document).ready(function(){
     },
     function () {
     $(this).animate({opacity:'0'});
-})  
-
+})
+});
 $(document).ready(function(){
     $('#submission').submit(function(event){
-        var display1 = $('input#name').val();
+        var display1 = $('input:first').val();
         var display2 = $('input#mail').val();
-            alert (display1 + ' we have received your message. Thank you for reaching out to us.');
+        if(display1 && display2 != ''){
+            alert ('Thank you ' + display1 + ' your subscription is well received');
+        }else {
+            alert ('Invalid input');
         }
         event.preventDefault();
     })
